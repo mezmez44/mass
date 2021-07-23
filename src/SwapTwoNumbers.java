f6984b8ec2986ec9a305fba6e964fe8bbc56c7dd
mezmez44 <mezmez44@gmail.com>
2021-07-23 11:35:37 -0400

    swaptwonumbers

--- src/SwapTwoNumbers.java
+++ src/SwapTwoNumbers.java
@@ -1,2 +1,15 @@
 public class SwapTwoNumbers {
+
+    public static void main(String[]args){
+        int a=7;
+        int b=9;
+        System.out.println(swapNbr(a,b));
+    }
+    public static String swapNbr(int a,int b){
+        int c=0;
+         c=a;
+         a=b;
+         b=c;
+        return "a= "+a+"\n"+"b= "+b;
+    }
 }
